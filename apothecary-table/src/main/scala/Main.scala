84ba98228546e9a697d064e1629fa17c038b37b9
mariusz <brugmar@gmail.com>
2016-07-05 23:34:40 +0200

    Hello World Akka!

--- apothecary-table/src/main/scala/Main.scala
+++ apothecary-table/src/main/scala/Main.scala
@@ -1,3 +1,18 @@
+import PrintActor.Print
+import akka.actor.{Actor, ActorSystem, Props}
+
+object PrintActor {
+  case class Print(message: String)
+}
+
+class PrintActor extends Actor {
+  override def receive: Receive = {
+    case Print(msg) => println(msg)
+  }
+}
+
 object Main extends App {
-  println("Hello world!")
+  val system = ActorSystem("PrintIt")
+  val printActor = system.actorOf(Props[PrintActor])
+  printActor ! Print("Hello World Akka!")
 }
