abd0645a58340272c9a96656d7eba7f4bcb07c13
mariusz <brugmar@gmail.com>
2016-07-06 23:21:04 +0200

    Akka http play

--- apothecary-table/src/main/scala/Main.scala
+++ apothecary-table/src/main/scala/Main.scala
@@ -1,18 +1,32 @@
-import PrintActor.Print
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import spray.json.DefaultJsonProtocol
 
-object PrintActor {
-  case class Print(message: String)
+case class TempValue(value: String)
+
+trait ServiceJsonProtoocol extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val customerProtocol = jsonFormat1(TempValue)
 }
 
-class PrintActor extends Actor {
-  override def receive: Receive = {
-    case Print(msg) => println(msg)
-  }
-}
+object Main extends App with ServiceJsonProtoocol {
+  implicit val system = ActorSystem("ApothecaryTableActorSystem")
+  implicit val materializer = ActorMaterializer()
 
-object Main extends App {
-  val system = ActorSystem("PrintIt")
-  val printActor = system.actorOf(Props[PrintActor])
-  printActor ! Print("Hello World Akka!")
+  val route =
+    path("hello") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      } ~
+      post {
+        entity(as[TempValue]){
+          case TempValue(str) => complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, s"You send value [$str]"))
+        }
+      }
+    }
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 }
